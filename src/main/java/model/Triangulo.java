bae11df8a905e11e361110784acc83139b0e0813
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-03-21 15:39:07 -0300

    add classes Quadrado e Triangulo

--- src/main/java/model/Triangulo.java
+++ src/main/java/model/Triangulo.java
@@ -1,2 +1,48 @@
-package model;public class Triangulo {
+package model;
+
+public class Triangulo {
+
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Triangulo() {
+
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcularAreaTriangulo(){
+        return (base*altura)/2.0;
+    }
+
+    public Triangulo trianguloDeMenorArea(Triangulo triangulo1, Triangulo triangulo2) {
+        if (triangulo1.calcularAreaTriangulo() == triangulo2.calcularAreaTriangulo()){
+            return null;
+        } else if (triangulo1.calcularAreaTriangulo() < triangulo2.calcularAreaTriangulo()){
+            return triangulo1;
+        } else {
+            return triangulo2;
+        }
+    }
+
 }
+
