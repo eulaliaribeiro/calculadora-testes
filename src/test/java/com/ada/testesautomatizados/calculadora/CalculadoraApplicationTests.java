07838dcb710387bff82bcb67a273a77b73058237
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-03-21 15:41:28 -0300

    add CalculadoraServiceTeste

--- src/test/java/com/ada/testesautomatizados/calculadora/CalculadoraApplicationTests.java
+++ src/test/java/com/ada/testesautomatizados/calculadora/CalculadoraApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CalculadoraApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- src/test/java/service/CalculadoraServiceTeste.java
+++ src/test/java/service/CalculadoraServiceTeste.java
@@ -1,2 +1,53 @@
-package service;public class CalculadoraServiceTeste {
+package service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraServiceTeste {
+
+    CalculadoraService calculadoraService = new CalculadoraService();
+    int a;
+    int b;
+
+    @Before
+    public void before() {
+        a = 8;
+        b = 2;
+    }
+
+    @Test
+    public void deveSomarCorretamente() {
+        //Given
+        //When
+        int resultado = calculadoraService.somar(a, b);
+
+        //Then
+        Assert.assertEquals(a + b, resultado);
+    }
+
+
+    @Test
+    public void deveSubtrairCorretamente() {
+        int resultado = calculadoraService.subtrair(a, b);
+
+        Assert.assertEquals(a-b, resultado);
+
+    }
+
+    @Test
+    public void deveDividirCorretamente() {
+        int resultado = calculadoraService.dividir(a, b);
+
+        Assert.assertEquals(a/b, resultado);
+    }
+
+    @Test
+    public void deveMultiplicarCorretamente() {
+        int resultado = calculadoraService.multiplicar(a, b);
+
+        Assert.assertEquals(a*b, resultado);
+
+    }
+
 }
