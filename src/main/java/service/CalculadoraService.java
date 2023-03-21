69b55469e9a17f91cafc1cb832d543019be20a81
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-03-21 15:38:40 -0300

    add CalculadoraService

--- src/main/java/service/CalculadoraService.java
+++ src/main/java/service/CalculadoraService.java
@@ -1,2 +1,23 @@
-package service;public class CalculadoraService {
+package service;
+
+// config junit 4
+// criar arquivos teste nas pastas corretas
+// testar
+
+public class CalculadoraService {
+    public int somar(int a, int b) {
+        return a+b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a-b;
+    }
+
+    public int dividir(int a, int b) {
+        return a/b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a*b;
+    }
 }
