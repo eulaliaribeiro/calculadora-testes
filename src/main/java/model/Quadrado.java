3039cc548509ee84caa18b7e17f16f6971939c0e
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-03-21 15:41:54 -0300

    add classe Quadrado e TrianguloTeste

--- src/main/java/model/Quadrado.java
+++ src/main/java/model/Quadrado.java
@@ -1,2 +1,37 @@
-package model;public class Quadrado {
+package model;
+
+public class Quadrado {
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public Quadrado() {
+
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double calcularAreaQuadrado(){
+        return lado*lado;
+    }
+
+    public Quadrado quadradoDeMenorArea(Quadrado quadrado1, Quadrado quadrado2) {
+        if (quadrado1.calcularAreaQuadrado() == quadrado2.calcularAreaQuadrado()){
+            return null;
+        } else if (quadrado1.calcularAreaQuadrado() < quadrado2.calcularAreaQuadrado()){
+            return quadrado1;
+        } else {
+            return quadrado2;
+        }
+    }
+
 }
--- src/test/java/model/TrianguloTeste.java
+++ src/test/java/model/TrianguloTeste.java
@@ -1,2 +1,56 @@
-package model;public class TrianguloTeste {
+package model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TrianguloTeste {
+
+    Triangulo triangulo = new Triangulo();
+    Triangulo triangulo1 = new Triangulo();
+    Triangulo triangulo2 = new Triangulo();
+
+
+    @Test
+    public void trianguloDeMenorAreaDeveTerAreaMenorQueDoTriangulo2() {
+        triangulo1.setBase(3);
+        triangulo1.setAltura(4);
+        triangulo2.setBase(6);
+        triangulo2.setAltura(8);
+
+        Triangulo resultado = triangulo.trianguloDeMenorArea(triangulo1, triangulo2);
+
+        Assert.assertTrue(resultado.calcularAreaTriangulo() < triangulo2.calcularAreaTriangulo());
+
+    }
+
+    @Test
+    public void trianguloDeMenorAreaDeveTerAreaMenorQueDoTriangulo1() {
+        triangulo1.setBase(6);
+        triangulo1.setAltura(8);
+        triangulo2.setBase(3);
+        triangulo2.setAltura(4);
+
+        Triangulo resultado = triangulo.trianguloDeMenorArea(triangulo1, triangulo2);
+
+        Assert.assertFalse(resultado.calcularAreaTriangulo() > triangulo2.calcularAreaTriangulo());
+
+    }
+
+    @Test
+    public void deveRetornarNuloQuandoAsAreasDosTriangulosSaoIguais() {
+        triangulo1.setBase(3);
+        triangulo1.setAltura(4);
+        triangulo2.setBase(3);
+        triangulo2.setAltura(4);
+
+        Triangulo resultado = triangulo.trianguloDeMenorArea(triangulo1, triangulo2);
+
+        Assert.assertEquals(null, resultado);
+
+    }
+
+
+
+
+
 }
